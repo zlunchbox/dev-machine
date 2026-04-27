25f77dc8c676333817db12acbba51204416edbef
Zachary Luettgen <zluettgen@gmail.com>
2026-04-26 23:16:43 -0400

    add connect, status scripts; add roles for rust and tauri install

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,17 @@ output "instance_public_ip" {
   value       = aws_instance.dev_machine.public_ip
 }
 
+output "instance_id" {
+  description = "Instance ID of the dev machine"
+  value       = aws_instance.dev_machine.id
+}
+
+output "aws_region" {
+  description = "AWS region the dev machine is deployed in"
+  value       = var.aws_region
+}
+
 output "ssh_command" {
   description = "SSH command to connect to the dev machine"
-  value       = "ssh -i ~/.ssh/dev-machine-key ubuntu@${aws_instance.dev_machine.public_ip}"
+  value       = "ssh -A -i ~/.ssh/dev-machine-key ubuntu@${aws_instance.dev_machine.public_ip}"
 }
